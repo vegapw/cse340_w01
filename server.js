fbd9797afd303bc76a56e6f6318f1259cc743ef9
Fredy Vega <fredyvega@Fredys-MacBook-Air.local>
2024-11-26 18:51:57 -0500

    W03 activities - Db Connection

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,9 +24,12 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //Index Route
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.render('index', {title:"Home"});
-});
+});*/
+app.get('/', baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
